6f0108124aba0f8ea2a36c03b3676447cc633954
Gonghua Li <ligonghua@mail.kiz.ac.cn>
2020-01-28 14:17:40 +0800

    improve convenience

--- Makefile.bat
+++ Makefile.bat
@@ -17,5 +17,13 @@ gcc ./src/singleGeneKI_gurobi.c -m64 -g -o ./bin/win32/singleGeneKI_gurobi.exe -
 gcc ./src/singleMetKO_gurobi.c -m64 -g -o ./bin/win32/singleMetKO_gurobi.exe -IC:\gurobi652\win64\include  -I.\include -LC:\gurobi652\win64\lib -lm -lgurobi65 -lpthread -O3
 gcc ./src/doubleMetKO_gurobi.c -m64 -g -o ./bin/win32/doubleMetKO_gurobi.exe -IC:\gurobi652\win64\include  -I.\include -LC:\gurobi652\win64\lib -lm -lgurobi65 -lpthread -O3
 
-gcc ./src/FVA_gurobi_mat.c -m64 -g -o FVA_gurobi_mat -IC:\gurobi652\win64\include -I.\include -I"C:\Program Files\MATLAB\R2013a\extern\include" -LC:\gurobi652\win64\lib -L"C:\Program Files\MATLAB\R2013a\bin\win64" -lmat -leng -lmx -lgurobi65 -lpthread -lm -O3
+gcc ./src/FVA_gurobi_mat.c -m64 -g -o ./bin/win32/FVA_gurobi_mat -IC:\gurobi652\win64\include -I.\include -I"C:\Program Files\MATLAB\R2013a\extern\include" -LC:\gurobi652\win64\lib -L"C:\Program Files\MATLAB\R2013a\bin\win64" -lmat -leng -lmx -lgurobi65 -lpthread -lm -O3
 
+# cplex win64
+gcc ./src/FVA_cplex_mat.c -m64 -g -o ./bin/win32/FVA_cplex_mat -ID:/programms/IBM/ILOG/CPLEX_Studio126/cplex/include/ -I./include -ID:/programms/Matlab/R2017a/extern/include -LD:/programms/IBM/ILOG/CPLEX_Studio126/cplex/lib/x64_windows_vs2010/stat_mda -LD:/programms/Matlab/R2017a/bin/win64 -lmat -leng -lmx -lcplex1260 -lpthread -lm -O3
+
+gcc ./src/singleGeneKO_cplex.c -m64 -g -o ./bin/win32/singleGeneKO_cplex -ID:/programms/IBM/ILOG/CPLEX_Studio126/cplex/include/ -I./include -ID:/programms/Matlab/R2017a/extern/include -LD:/programms/IBM/ILOG/CPLEX_Studio126/cplex/lib/x64_windows_vs2010/stat_mda -LD:/programms/Matlab/R2017a/bin/win64 -lmat -leng -lmx -lcplex1260 -lpthread -lm -O3
+
+gcc ./src/doubleGeneKO_cplex.c -m64 -g -o ./bin/win32/doubleGeneKO_cplex -ID:/programms/IBM/ILOG/CPLEX_Studio126/cplex/include/ -I./include -ID:/programms/Matlab/R2017a/extern/include -LD:/programms/IBM/ILOG/CPLEX_Studio126/cplex/lib/x64_windows_vs2010/stat_mda -LD:/programms/Matlab/R2017a/bin/win64 -lmat -leng -lmx -lcplex1260 -lpthread -lm -O3
+
+gcc ./src/singleGeneKI_cplex.c -m64 -g -o ./bin/win32/singleGeneKI_cplex -ID:/programms/IBM/ILOG/CPLEX_Studio126/cplex/include/ -I./include -ID:/programms/Matlab/R2017a/extern/include -LD:/programms/IBM/ILOG/CPLEX_Studio126/cplex/lib/x64_windows_vs2010/stat_mda -LD:/programms/Matlab/R2017a/bin/win64 -lmat -leng -lmx -lcplex1260 -lpthread -lm -O3
